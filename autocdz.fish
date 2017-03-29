f5ace1c8075724bcf1df42d04c336203d6af3f85
ymmtmdk <ymmtmdk@gmail.com>
2017-03-29 21:01:39 +0900

    Update autocdz.fish

--- autocdz.fish
+++ autocdz.fish
@@ -1,9 +1,7 @@
 function autocdz
-  function on_fish_postexec --on-event fish_postexec
-    if test -d $argv
-      cd $argv
-    else if z_jump $argv
-      echo cd $PWD
-    end
+  if test -d $argv
+    cd $argv
+  else if z_jump $argv
+    echo cd $PWD
   end
 end
