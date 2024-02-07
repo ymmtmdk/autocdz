20cdc923f99809ae71d72820e8f408fd380dc4fa
ymmtmdk <ymmtmdk@gmail.com>
2024-02-07 11:19:33 +0900

    NC

--- autocdz.fish
+++
@@ -1,9 +0,0 @@
-function autocdz
-  if test (count $argv) -gt 1
-    echo "fish: Unknown command '$argv[1]'" >&2
-  else if test -d $argv
-    cd $argv
-  else if __z $argv
-    echo cd $PWD
-  end
-end
