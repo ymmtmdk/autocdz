6a3d27a8192fad2b0b10d17e54864a77b2b853c5
ymmtmdk <ymmtmdk@gmail.com>
2024-02-18 15:56:04 +0900

    NC

--- conf.d/autocdz.fish
+++ conf.d/autocdz.fish
@@ -1,14 +1,22 @@
+if test -z "$Z_DATA"
+  set -U Z_DATA "$HOME/.z"
+end
+
+if test ! -f "$Z_DATA"
+  touch "$Z_DATA"
+end
+
 function __z_add -d "Add PATH to .z file"
   set -l path (dirname (status -f))
   set -l tmpfile (mktemp $Z_DATA.XXXXXX)
 
   if test -f $tmpfile
-    zadd "$PWD" (date +%s) $Z_DATA ^ /dev/null > $tmpfile
+    zadd "$argv" (date +%s) $Z_DATA ^ /dev/null > $tmpfile
     mv -f $tmpfile $Z_DATA
   end
 end
 
-function __z_move -d "Jump to a recent directory."
+function __z_move -d "Jump to a recent directory"
   set -g path (dirname (status -f))
   set -l target
 
@@ -24,7 +32,15 @@ function __z_move -d "Jump to a recent directory."
   end
 
   pushd "$target"
-  return 0
+  echo cd $target
+end
+
+function __z_on_variable_pwd --on-variable PWD
+  __z_add $PWD
+end
+
+function fish_command_not_found
+  autocdz $argv
 end
 
 function autocdz
@@ -32,8 +48,8 @@ function autocdz
     echo "fish: Unknown command '$argv[1]'" >&2
   else if test -d $argv
     cd $argv
-  else if __z_move $argv
-    echo cd $PWD
+  else
+    __z_move $argv
   end
 end
 
